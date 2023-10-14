18ba0f2ba8ac573a55cec93e6ebc0ec488c71ec1
admin <loookintome@gmail.com>
2023-10-14 12:17:35 +0900

    First commit

---
+++ lesson_0001/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        byte varbyte = 1;
+        char varchar = 'c';
+        float varfloat = 1.11F;
+        double vardouble = 1.11D;
+        boolean varboolean = false;
+        int varint = 1;
+        short varshort = 1;
+        long varlong = 1;
+    }
+}
